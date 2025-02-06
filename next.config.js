a7b2b3d17287d92932e9371b87b3e4575348beed
EnriqueJ97 <enriquejimenezcruz@gmail.com>
2025-02-06 02:05:13 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/eMotionTrackDemo',
+  assetPrefix: '/eMotionTrackDemo',
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+}
+
+module.exports = nextConfig
